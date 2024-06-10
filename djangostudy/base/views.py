43753f73564770014000ea559ed7af5f6e4325a6
Adedayo UWensuyi <onehpee@yahoo.com>
2024-06-09 22:17:38 -0500

    Passing in Views into home and rendering

--- djangostudy/base/views.py
+++ djangostudy/base/views.py
@@ -2,16 +2,15 @@ from django.shortcuts import render
 
 
 rooms = [
-    {'id': 1, 'name': 'Lets learn python'}
-    {'id': 2, 'name': 'Design with me'}
-    {'id': 3, 'name': 'Frontend Developers'}
-
-
+    {'id': 1, 'name': 'Lets learn python'},
+    {'id': 2, 'name': 'Design with me'},
+    {'id': 3, 'name': 'Frontend Developers'},
 ]
 
 
 def home(request):
-    return render(request, 'home.html', {'rooms': rooms})
+    context = {'rooms' : rooms}
+    return render(request, 'home.html', context)
 
 def room(request):
     return render(request, 'room.html')
