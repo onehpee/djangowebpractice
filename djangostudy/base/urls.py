586ce66a85eeacece2288c63b691e3ce1b99afad
Adedayo UWensuyi <onehpee@yahoo.com>
2024-06-09 22:40:14 -0500

    Adding dynamic values to url to access rooms

--- djangostudy/base/urls.py
+++ djangostudy/base/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
      path('', views.home, name="home"),
-     path('room/', views.room, name="room"),
+     # pk is a dynamic value it stands for primary key
+     path('room/<str:pk>', views.room, name="room"),
 ]
--- djangostudy/base/views.py
+++ djangostudy/base/views.py
@@ -12,7 +12,12 @@ def home(request):
     context = {'rooms' : rooms}
     return render(request, 'base/home.html', context)
 
-def room(request):
+def room(request, pk):
+    room = None
+    for i in rooms:
+        if i['id'] == int(pk):
+            room = i
+    context = {'room': room}
     return render(request, 'base/room.html')
 
-# Create your views here.
+
