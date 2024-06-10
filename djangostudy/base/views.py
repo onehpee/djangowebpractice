59d00e283b1170e7a640630d4ba35b0603de8587
Adedayo UWensuyi <onehpee@yahoo.com>
2024-06-09 22:07:31 -0500

    Views

--- djangostudy/base/views.py
+++ djangostudy/base/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 
 
+rooms = [
+    {'id': 1, 'name': 'Lets learn python'}
+    {'id': 2, 'name': 'Lets learn python'}
+    {'id': 3, 'name': 'Lets learn python'}
+
+
+]
+
+
 def home(request):
     return render(request, 'home.html')
 
