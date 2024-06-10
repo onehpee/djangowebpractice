4c630bbdadc37e61a5f7f761b116aa3e118c0067
Adedayo UWensuyi <onehpee@yahoo.com>
2024-06-09 22:09:07 -0500

    Passing in Views

--- djangostudy/base/views.py
+++ djangostudy/base/views.py
@@ -3,15 +3,15 @@ from django.shortcuts import render
 
 rooms = [
     {'id': 1, 'name': 'Lets learn python'}
-    {'id': 2, 'name': 'Lets learn python'}
-    {'id': 3, 'name': 'Lets learn python'}
+    {'id': 2, 'name': 'Design with me'}
+    {'id': 3, 'name': 'Frontend Developers'}
 
 
 ]
 
 
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'home.html', {'rooms': rooms})
 
 def room(request):
     return render(request, 'room.html')
