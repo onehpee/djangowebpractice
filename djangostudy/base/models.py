859651f1ccbbb4899309eae01cb87a7bacfd89ee
Adedayo UWensuyi <onehpee@yahoo.com>
2024-06-19 23:04:36 -0500

    making a model for message

--- djangostudy/base/models.py
+++ djangostudy/base/models.py
@@ -1,7 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.deletion import CASCADE
 
 # Create your models here.
-
+class Topic(models.Model):
+    name = models.CharField(max_length=200)
+    
+    def __str__(self):
+        return self.name
 
 class Room(models.Model):
     #host =
@@ -14,3 +20,14 @@ class Room(models.Model):
     
     def __str__(self):
         return self.name
+    
+    
+class Messsage(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    body = models.TextField
+    update = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.body[0:50]
