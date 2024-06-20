2d55a116649e4472fb439fa03ef0c096ca10725d
Adedayo UWensuyi <onehpee@yahoo.com>
2024-06-19 23:08:10 -0500

    making a model for Topic

--- djangostudy/base/models.py
+++ djangostudy/base/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.db.models.deletion import CASCADE
 
 # Create your models here.
 class Topic(models.Model):
@@ -10,8 +9,8 @@ class Topic(models.Model):
         return self.name
 
 class Room(models.Model):
-    #host =
-    #topic =
+    host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
     #participants =
