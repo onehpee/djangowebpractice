9b5dd67849b7a71b2d013cdc7acccad41cdf265c
Adedayo UWensuyi <onehpee@yahoo.com>
2024-06-21 21:01:32 -0500

    Rendering out the items

--- djangostudy/base/admin.py
+++ djangostudy/base/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import Room
+from .models import Room, Topic, Messsage
 
 admin.site.register(Room)
+admin.site.register(Topic)
+admin.site.register(Messsage)
+
 # Register your models here.
