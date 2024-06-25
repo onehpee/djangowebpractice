7f523c9745228e36f469494cc1a05123fb9153e0
Adedayo UWensuyi <onehpee@yahoo.com>
2024-06-25 00:03:32 -0500

    CRUD - Delete rooms - views, url, home

--- djangostudy/base/urls.py
+++ djangostudy/base/urls.py
@@ -9,5 +9,7 @@ urlpatterns = [
      
      path('create-room/', views.createRoom, name="create-room"),
      path('update-room/<str:pk>/', views.updateRoom, name="update-room"),
+     path('delete-room/<str:pk>/', views.deleteRoom, name="delete-room"),
+
 
 ]
