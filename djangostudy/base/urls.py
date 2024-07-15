85c3866c3c31d68001513332d42a71e590a1c713
Adedayo UWensuyi <onehpee@yahoo.com>
2024-07-15 00:04:05 -0500

    Account login register auth

--- djangostudy/base/urls.py
+++ djangostudy/base/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 urlpatterns = [
      path('login/', views.loginPage, name="login"),
+     path('logout/', views.logoutUser, name="logout"),
+
      path('', views.home, name="home"),
      # pk is a dynamic value it stands for primary key
      path('room/<str:pk>', views.room, name="room"),
