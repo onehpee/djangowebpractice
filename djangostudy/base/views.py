50ee7428562fd8b498b80b6d1a51c1348417b805
Adedayo UWensuyi <onehpee@yahoo.com>
2024-07-19 22:54:32 -0500

    Account login register auth

--- djangostudy/base/views.py
+++ djangostudy/base/views.py
@@ -78,7 +78,7 @@ def updateRoom(request, pk):
     room = Room.objects.get(id=pk)
     form = RoomForm(instance=room)
     
-    if request.user != room.user:
+    if request.user != room.host:
         return HttpResponse("Your are not allowed here!!")
     
     if request.method == 'POST':
