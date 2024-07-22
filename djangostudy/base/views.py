7f01ab9c3bf9711bf047ca4c5c7e553d57187375
Adedayo UWensuyi <onehpee@yahoo.com>
2024-07-22 17:07:18 -0500

    HTML for room

--- djangostudy/base/views.py
+++ djangostudy/base/views.py
@@ -77,7 +77,8 @@ def home(request):
 
 def room(request, pk):
     room = Room.objects.get(id=pk)
-    context = {'room': room}
+    room_messages = room.message_set.all()
+    context = {'room': room, 'messages':room_messages}
     return render(request, 'base/room.html', context)
 
 @login_required(login_required='/login')
